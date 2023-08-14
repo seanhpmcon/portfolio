284ce3f6ff67338b90a476f7c4b94e9d8ef92fbc
seanhpmcon <seanhpmcon@gmail.com>
2023-08-14 14:50:38 +0000

    added the code to display my name on the main page

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'NextAppName',
+        name: 'Portfolio',
         exec_mode: 'cluster',
         instances: 1, // Or a number of instances
         script: 'node_modules/next/dist/bin/next',
