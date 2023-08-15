1c11dc376171f33f34d543b9f749a1883efd3f04
seanhpmcon <seanhpmcon@gmail.com>
2023-08-15 13:03:02 +0000

    changes until the favicon change

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div>
-      <h1>Sean Harricharan</h1>
+    <div className="flex justify-center items-center min-h-screen bg-gray-800 text-white">
+      <div className="text-center">
+        <p className="font-serif font-bold text-4xl">Sean Harricharan.</p>
+      </div>
     </div>
   )
 }
